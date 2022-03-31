305b2de16d070696054c69caf477c5757c064d67
dan.reed <dan.reed@vistair.com>
2022-03-31 12:46:56 +0100

    clear button works

--- src/components/DoodleArea/DoodleArea.component.tsx
+++ src/components/DoodleArea/DoodleArea.component.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 import Sketch from 'react-p5';
 import p5Types from 'p5';
+import './DoodleArea.styles.css';
+// import p5 from 'p5';
 
 interface DoodleAreaProps {}
 
 const DoodleArea: React.FC<DoodleAreaProps> = (props: DoodleAreaProps) => {
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
+  const [p5Instance, setP5Instance]: any = useState(null);
+
+  // --------------------------------------------------
+  // Canvas callbacks
+  // --------------------------------------------------
+
+  const setupCanvas = (p5: p5Types, canvasParentRef: Element) => {
     p5.createCanvas(500, 500).parent(canvasParentRef);
+
+    // set a reference to the instance of p5 so it can be used later
+    // outside of the Sketch component.
+    setP5Instance(p5);
   };
 
-  const draw = (p5: p5Types) => {
-    p5.strokeWeight(15);
+  const drawOnCanvas = (p5: p5Types) => {
+    p5.strokeWeight(5);
     p5.stroke(0);
 
     // If mouse is pressed, draw line between previous and current mouse positions
@@ -19,7 +31,32 @@ const DoodleArea: React.FC<DoodleAreaProps> = (props: DoodleAreaProps) => {
     }
   };
 
-  return <Sketch setup={setup} draw={draw} />;
+  const clearCanvas = () => {
+    p5Instance.background(255);
+  };
+
+  // --------------------------------------------------
+  // Components
+  // --------------------------------------------------
+
+  const ClearButton = () => {
+    return <button onClick={() => clearCanvas()}>Clear</button>;
+  };
+
+  // --------------------------------------------------
+  // Render
+  // --------------------------------------------------
+
+  return (
+    <div className="doodle-area">
+      <div className="canvas">
+        <Sketch setup={setupCanvas} draw={drawOnCanvas} />
+      </div>
+      <div className="doodle-info">
+        <ClearButton />
+      </div>
+    </div>
+  );
 };
 
 export default DoodleArea;
